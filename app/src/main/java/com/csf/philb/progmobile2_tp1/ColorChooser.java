a55d579bcab7cb3bdd1015956c071c91266b48af
Philippe Boutin-Therrien <philboutin@hotmail.com>
2017-01-16 14:19:42 -0500

    Model classes basic code

---
+++ app/src/main/java/com/csf/philb/progmobile2_tp1/ColorChooser.java
@@ -0,0 +1,10 @@
+package com.csf.philb.progmobile2_tp1;
+
+public class ColorChooser {
+
+    private int color;
+
+    private int getColor(){
+        return color;
+    }
+}
---
+++ app/src/main/java/com/csf/philb/progmobile2_tp1/PossibleChoices.java
@@ -0,0 +1,12 @@
+package com.csf.philb.progmobile2_tp1;
+
+public enum PossibleChoices {
+
+    RED,
+    PINK,
+    PURPLE,
+    BLUE,
+    GREEN,
+    BROWN,
+    ORANGE
+}
